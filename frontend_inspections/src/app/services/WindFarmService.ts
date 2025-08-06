58107f59f712c036e900837bc8bcd986a813758f
oscar1234 <oscarizquierd224@gmail.com>
2025-08-06 23:56:32 +0200

    Create new WindFarm service file to make http request

---
+++ frontend_inspections/src/app/services/WindFarmService.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from '@angular/core';
+import { TotalCountWTGPiloted} from '../models/total-wtg-pilotados.model';
+import { TotalCountWTGInspections } from '../models/total-wtg-inspeccionados.model';
+import { WindFarm } from '../models/windfarm.model';
+import { Observable } from 'rxjs';
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class WindFarmService {
+
+    private url = 'http://localhost:8000';
+
+    constructor(private http: HttpClient) {
+
+    }
+
+    createNewWindFarm(formData: any): Observable<any> {
+        return this.http.post(`${this.url}/add_windFarm`, formData);
+    }
+}
